d179b70a1d07c3080fb1c62cee8f3a0ee9978769
Robert Pyke <robert.j.pyke@gmail.com>
2012-03-18 14:51:26 +1000

    Moved map code around so that the controller and view code is correctly split.

--- app/Controller/ToolsController.php
+++ app/Controller/ToolsController.php
@@ -24,8 +24,37 @@ class ToolsController extends AppController {
 	 * @return void
 	 */
 	public function map() {
-		// Pass through to view
 		$this->layout = false;
+
+		$map_request = ms_newOwsRequestObj();
+		$map_request->loadparams();
+
+		$map_path = realpath(APP.'/Lib/map');
+		$map_file = null;
+		if (array_key_exists('MAP', $this->request->query)) {
+			$map_file = $this->request->query['MAP'];
+		} else {
+			throw new BadRequestException(__('Invalid query. Missing MAP query parameter'));
+		}
+		
+		$map = ms_newMapObj(realpath($map_path.'/'.$map_file));
+		$map->loadOWSParameters($map_request);
+
+		$data = null;
+		if (array_key_exists('DATA', $this->request->query)) {
+			$data = $this->request->query['DATA'];
+		} else {
+			throw new BadRequestException(__('Invalid query. Missing DATA query parameter'));
+		}
+
+		$layer = $map->getLayerByName('TEMPDATA');
+		$layer->set('data', $data);
+
+		$map_image = $map->draw();
+
+		// Pass the map image through to view
+		$this->set('map_image', $map_image);
+
 	}
 
 }
--- app/View/Tools/map.ctp
+++ app/View/Tools/map.ctp
@@ -1,29 +1,5 @@
 <?php
 
-// Display a map as an inline image
-$inline = true;
-
-$mapRequest = ms_newOwsRequestObj();
-$mapRequest->loadparams();
-
-$map_path = realpath(APP.'/Lib/map');
-$mapfile = null;
-if (array_key_exists('MAP', $this->request->query)) {
-	$mapfile = $this->request->query['MAP'];
-} else {
-	throw new BadRequestException(__('Invalid MAP query parameter'));
-}
-$map = ms_newMapObj(realpath($map_path.'/'.$mapfile));
-$map->loadOWSParameters($mapRequest);
-
-$layer = $map->getLayerByName('TEMPDATA');
-$data = $_GET['DATA'];
-$layer->set('data', $data);
-
-$map_image = $map->draw();
-if ($inline) {
-    header('Content-Type: image/png');
-    $map_image->saveImage('');
-    exit;
-}
-$image_url = $map_image->saveWebImage();
+header('Content-Type: image/png');
+$map_image->saveImage('');
+exit;
